aad695507f01a6d854edfb75eaeaedb91d576254
Scott Pullen <s.pullen05@gmail.com>
2014-11-30 12:32:41 -0500

    adding users table

---
+++ app/database/migrations/2014_11_30_172741_create_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+
+	public function up() {
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('email');
+			$table->string('password', 60);
+			$table->rememberToken();
+			$table->timestamps();
+		});
+
+		Schema::table('users', function($table) {
+			$table->unique('email');
+		});
+	}
+
+	public function down() {
+		Schema::table('users', function($table) {
+			$table->dropUnique('users_email_unique');
+		});
+
+		Schema::drop('users');
+	}
+
+}
