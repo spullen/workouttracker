f32cc94dbe54608479fc363f4ee1e2fc4cf374af
Scott Pullen <s.pullen05@gmail.com>
2014-11-30 14:12:21 -0500

    adding goal_workouts table

---
+++ app/database/migrations/2014_11_30_190910_create_goal_workouts.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoalWorkouts extends Migration {
+
+	public function up() {
+		Schema::create('goal_workouts', function($table) {
+			$table->integer('goal_id')->unsigned();
+			$table->foreign('goal_id')->references('id')->on('goals');
+			$table->integer('workout_id')->unsigned();
+			$table->foreign('workout_id')->references('id')->on('workouts');
+		});
+	}
+
+	public function down() {
+		Schema::drop('goal_workouts');
+	}
+
+}
