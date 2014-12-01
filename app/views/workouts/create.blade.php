9fa981681c3c902b4b098cd0650f8189018c2167
Scott Pullen <s.pullen05@gmail.com>
2014-11-30 22:04:49 -0500

    adding duration to workout form, and correcting the classes for the select fields

--- app/views/workouts/create.blade.php
+++ app/views/workouts/create.blade.php
@@ -5,25 +5,31 @@
     <div class="form-group">
       {{ Form::label('activity_id', 'Activity Type', array('class' => 'control-label')) }}
       <div>
-        {{ Form::select('activity_id', WorkoutHelpers::activities(), array('class' => 'form-control')) }}
+        {{ Form::select('activity_id', WorkoutHelpers::activities(), $workout->activity_id, array('class' => 'form-control')) }}
       </div>
     </div>
     <div class="form-group">
       {{ Form::label('metric', 'Metric', array('class' => 'control-label')) }}
       <div>
-        {{ Form::select('metric', WorkoutHelpers::metrics()) }}
+        {{ Form::select('metric', WorkoutHelpers::metrics(), $workout->metric, array('class' => 'form-control')) }}
       </div>
     </div>
     <div class="form-group">
       {{ Form::label('amount', 'Amount', array('class' => 'control-label')) }}
       <div>
-        {{ Form::text('amount', Input::get('amount', 1), array('class' => 'form-control')) }}
+        {{ Form::text('amount', $workout->amount, array('class' => 'form-control')) }}
       </div>
     </div>
     <div class="form-group">
       {{ Form::label('start', 'Start') }}
       <div>
-        {{ Form::text('start', Input::get('start', null), array('class' => 'form-control datetimepicker')) }}
+        {{ Form::text('start', $workout->start, array('class' => 'form-control datetimepicker')) }}
+      </div>
+    </div>
+    <div class="form-group">
+      {{ Form::label('duration', 'Duration (in minutes)') }}
+      <div>
+        {{ Form::text('duration') }}
       </div>
     </div>
   {{ Form::close() }}
