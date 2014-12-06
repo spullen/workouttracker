6ec2bf95d930cac4e1f0805cf44f85d90f5d796c
Scott Pullen <s.pullen05@gmail.com>
2014-12-06 13:31:49 -0500

    adding ga

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -30,5 +30,17 @@
   <script src="{{ asset('js/bootstrap-datetimepicker.js') }}"></script>
   <script src="{{ asset('js/application.js') }}"></script>
 @show
+@if(App::environment() == "production")
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57389075-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+@endif
 </body>
 </html>
