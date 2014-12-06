a2e288581dd8d6e824dc54f11ebf2df4afafff3c
Scott Pullen <s.pullen05@gmail.com>
2014-12-05 22:44:30 -0500

    adding error styles to registration form

--- app/views/registration/create.blade.php
+++ app/views/registration/create.blade.php
@@ -12,35 +12,35 @@
   <form action="{{ action('register.store') }}" method="POST" class="form-horizontal">
     {{ Form::token() }}
     
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
       {{ Form::label('first_name', 'First name', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::text('first_name', Input::get('first_name'), array('class' => 'form-control')) }}
         {{ $errors->first('first_name', '<span class="help-block">:message</span>') }}
       </div>
     </div>
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
       {{ Form::label('last_name', 'Last name', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::text('last_name', Input::get('last_name'), array('class' => 'form-control')) }}
         {{ $errors->first('last_name', '<span class="help-block">:message</span>') }}
       </div>
     </div>
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
       {{ Form::label('email', 'Email', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::text('email', Input::get('email'), array('class' => 'form-control')) }}
         {{ $errors->first('email', '<span class="help-block">:message</span>') }}
       </div>
     </div>
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
       {{ Form::label('password', 'Password', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::password('password', array('class' => 'form-control')) }}
         {{ $errors->first('password', '<span class="help-block">:message</span>') }}
       </div>
     </div>
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('password_confirmation') ? 'has-error' : '' }}">
       {{ Form::label('password_confirmation', 'Password confirmation', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
