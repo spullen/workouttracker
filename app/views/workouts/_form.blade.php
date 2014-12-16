94579b805e932857e57d5c4665ffaacef3478ced
Scott Pullen <s.pullen05@gmail.com>
2014-12-15 22:28:05 -0500

    adding astericks for required workout fields

--- app/views/workouts/_form.blade.php
+++ app/views/workouts/_form.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group {{ $errors->has('amount') ? 'has-error' : '' }}">
-  {{ Form::label('amount', 'Amount', array('class' => 'control-label col-md-2')) }}
+  {{ Form::label('amount', 'Amount *', array('class' => 'control-label col-md-2')) }}
   <div class="col-md-6">
     {{ Form::text('amount', Input::get('amount'), array('class' => 'form-control')) }}
     {{ $errors->first('amount', '<span class="help-block">:message</span>') }}
   </div>
 </div>
 <div class="form-group {{ $errors->has('duration') ? 'has-error' : '' }}">
-  {{ Form::label('duration', 'Duration (in minutes)', array('class' => 'control-label col-md-2')) }}
+  {{ Form::label('duration', 'Duration (in minutes) *', array('class' => 'control-label col-md-2')) }}
   <div class="col-md-6">
     {{ Form::text('duration', Input::get('duration'), array('class' => 'form-control')) }}
     {{ $errors->first('duration', '<span class="help-block">:message</span>') }}
--- app/views/workouts/create.blade.php
+++ app/views/workouts/create.blade.php
@@ -12,14 +12,14 @@
 <div class="container">
   {{ Form::model($workout, ['method' => 'post', 'action' => 'WorkoutsController@store', 'class' => 'form-horizontal']) }}
     <div class="form-group {{ $errors->has('activity_id') ? 'has-error' : '' }}">
-      {{ Form::label('activity_id', 'Activity', array('class' => 'control-label col-md-2')) }}
+      {{ Form::label('activity_id', 'Activity *', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::select('activity_id', WorkoutHelpers::activities(), Input::get('activity_id'), array('class' => 'form-control')) }}
         {{ $errors->first('activity_id', '<span class="help-block">:message</span>') }}
       </div>
     </div>
     <div class="form-group {{ $errors->has('metric_id') ? 'has-error' : '' }}">
-      {{ Form::label('metric_id', 'Metric', array('class' => 'control-label col-md-2')) }}
+      {{ Form::label('metric_id', 'Metric *', array('class' => 'control-label col-md-2')) }}
       <div class="col-md-6">
         {{ Form::select('metric_id', WorkoutHelpers::metrics(Input::get('activity_id')), Input::get('metric_id'), array('class' => 'form-control')) }}
         {{ $errors->first('metric_id', '<span class="help-block">:message</span>') }}
