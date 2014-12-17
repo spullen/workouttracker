8d1f1c558c2547dde22c953084f2099395ccbf7f
Scott Pullen <s.pullen05@gmail.com>
2014-12-16 22:31:58 -0500

    adding met values to ActivityTableSeeder

--- app/database/seeds/ActivityTableSeeder.php
+++ app/database/seeds/ActivityTableSeeder.php
@@ -12,36 +12,36 @@ class ActivityTableSeeder extends Seeder {
 		$yards = Metric::firstOrCreate(array('name' => 'yards'));
 
 		// activities
-		$running = Activity::firstOrCreate(array('name' => 'Running'));
+		$running = Activity::firstOrCreate(array('name' => 'Running', 'met' => 7.0));
 		$running->metrics()->attach($distance);
 
-		$cycling = Activity::firstOrCreate(array('name' => 'Cycling'));
+		$cycling = Activity::firstOrCreate(array('name' => 'Cycling', 'met' => 8.0));
 		$cycling->metrics()->attach($distance);
 
-		$walking = Activity::firstOrCreate(array('name' => 'Walking'));
+		$walking = Activity::firstOrCreate(array('name' => 'Walking', 'met' => 3.5));
 		$walking->metrics()->attach($distance);
 		$walking->metrics()->attach($steps);
 
-		$hiking = Activity::firstOrCreate(array('name' => 'Hiking'));
+		$hiking = Activity::firstOrCreate(array('name' => 'Hiking', 'met' => 6.0));
 		$hiking->metrics()->attach($distance);
 
-		$push_ups = Activity::firstOrCreate(array('name' => 'Push Ups'));
+		$push_ups = Activity::firstOrCreate(array('name' => 'Push Ups', 'met' => 8.0));
 		$push_ups->metrics()->attach($count);
 
-		$pull_ups = Activity::firstOrCreate(array('name' => 'Pull Ups'));
+		$pull_ups = Activity::firstOrCreate(array('name' => 'Pull Ups', 'met' => 8.0));
 		$pull_ups->metrics()->attach($count);
 
-		$sit_ups = Activity::firstOrCreate(array('name' => 'Sit Ups'));
+		$sit_ups = Activity::firstOrCreate(array('name' => 'Sit Ups', 'met' => 8.0));
 		$sit_ups->metrics()->attach($count);
 
-		$crunches = Activity::firstOrCreate(array('name' => 'Crunches'));
+		$crunches = Activity::firstOrCreate(array('name' => 'Crunches', 'met' => 8.0));
 		$crunches->metrics()->attach($count);
 
-		$lifting = Activity::firstOrCreate(array('name' => 'Weight Lifting'));
+		$lifting = Activity::firstOrCreate(array('name' => 'Weight Lifting', 'met' => 6.0));
 		$lifting->metrics()->attach($count);
 		$lifting->metrics()->attach($reps);
 
-		$swimming = Activity::firstOrCreate(array('name' => 'Swimming'));
+		$swimming = Activity::firstOrCreate(array('name' => 'Swimming', 'met' => 7.0));
 		$swimming->metrics()->attach($laps);
 		$swimming->metrics()->attach($yards);
 	}
