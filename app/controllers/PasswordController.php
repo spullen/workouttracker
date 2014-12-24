e6f024a8d966838101df9b9531d8f696069d7c95
Scott Pullen <s.pullen05@gmail.com>
2014-12-23 22:09:33 -0500

    adding password update

--- app/controllers/PasswordController.php
+++ app/controllers/PasswordController.php
@@ -7,7 +7,29 @@ class PasswordController extends BaseController {
   }
 
   public function update() {
+    $user = Auth::user();
+    $data = Input::only('current_password', 'password', 'password_confirmation');
 
+    $validator = Validator::make($data, array(
+      'current_password' => array('required'),
+      'password' => array('required', 'min:8', 'confirmed')
+    ));
+
+    if($validator->fails()) {
+      return View::make('password.edit')->withErrors($validator);
+    }
+
+    if(!Auth::validate(array('email' => $user->email, 'password' => $data['current_password']))) {
+      Session::flash('alert', trans('validation.invalid_password'));
+      return View::make('password.edit');
+    }
+
+    $user->password = Hash::make($data['password']);
+    $user->save();
+
+    Session::flash('message', 'Successfully changed password.');
+
+    return Redirect::action('dashboard');
   }
 
 }
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Workout Tracker</title>
+  <title>getMoving.fitness</title>
 
   @section('stylesheets')
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
