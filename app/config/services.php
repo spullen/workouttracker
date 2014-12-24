3eb6bc7f92bb6e4fc993d0523ec4db3f3d554118
Scott Pullen <s.pullen05@gmail.com>
2014-12-24 12:09:13 -0500

    adding beanstalkd mq and adding env variable for madrill api

--- app/config/services.php
+++ app/config/services.php
@@ -2,25 +2,8 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Third Party Services
-	|--------------------------------------------------------------------------
-	|
-	| This file is for storing the credentials for third party services such
-	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
-	| default location for this type of information, allowing packages
-	| to have a conventional place to find your various credentials.
-	|
-	*/
-
-	'mailgun' => array(
-		'domain' => '',
-		'secret' => '',
-	),
-
 	'mandrill' => array(
-		'secret' => '',
+		'secret' => getenv('MANDRILL_API_KEY'),
 	),
 
 	'stripe' => array(
